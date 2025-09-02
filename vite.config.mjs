beddbd93c80a4790f37add18ca0268b2c1cd61c8
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-09-02 02:03:30 +0200

    Updated

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,6 @@
-// vite.config.mjs
 import { defineConfig } from 'vite';
 import { resolve, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
